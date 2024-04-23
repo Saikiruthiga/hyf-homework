4d52a927059b22c331b507a476ebcef4793a5198
Saikiruthiga <saikiruthiga06@gmail.com>
2024-04-23 11:26:09 +0200

    Feedback implemented

--- javascript/javascript1/week1/week1.js
+++ javascript/javascript1/week1/week1.js
@@ -1,68 +1,107 @@
 //Age-ify (A future age calculator)
 console.log("********** Age-ify (A future age calculator) *************");
-let yearOfBirth = 1991;
-let yearFuture = 2027;
-let age = yearFuture - yearOfBirth;
+const yearOfBirth = 1991;
+const yearFuture = 2027;
+const age = yearFuture - yearOfBirth;
 console.log("You will be " + age + " years old in " + yearFuture);
 
 //Goodboy-Oldboy (A dog age calculator)
-console.log("******************* Goodboy-Oldboy (A dog age calculator) **********");
-let dogYearOfBirth = 2017;
-let dogYearFuture = 2027;
+console.log(
+  "******************* Goodboy-Oldboy (A dog age calculator) **********"
+);
+const dogYearOfBirth = 2017;
+const dogYearFuture = 2027;
 let dogYear;
 let shouldShowResultInDogYears = false;
-if(shouldShowResultInDogYears){
-    dogYear = (dogYearFuture - dogYearOfBirth) * 7;
-    console.log("Your dog will be "+ dogYear + " dog years old in " +dogYearFuture);
+if (shouldShowResultInDogYears) {
+  dogYear = (dogYearFuture - dogYearOfBirth) * 7;
+} else {
+  dogYear = dogYearFuture - dogYearOfBirth;
 }
-else{
-    dogYear = (dogYearFuture - dogYearOfBirth);
-    console.log("Your dog will be "+ dogYear + " human years old in " +dogYearFuture);
+let yearType;
+if (shouldShowResultInDogYears) {
+  yearType = "dog";
+} else {
+  yearType = "human";
 }
 
+console.log(
+  "Your dog will be " +
+    dogYear +
+    " " +
+    yearType +
+    " years old in " +
+    dogYearFuture
+);
 
 //Housey pricey (A house price estimator)
 console.log("********** Housey pricey (A house price estimator) *********");
-let peterHouseDetails = [8,10,10,100, 2500000];
-let juliaHouseDetails = [5,11,8,70,1000000]
-let peterVolumeInMeters = peterHouseDetails[0]*peterHouseDetails[1]*peterHouseDetails[2];
-let juliaVolumeInMeters = juliaHouseDetails[0]*juliaHouseDetails[1]*juliaHouseDetails[2];
+const peterHouseWidth = 8;
+const peterHouseDepth = 10;
+const peterHouseHeight = 10;
+const peterGardenSize = 100;
+const peterPaidAmount = 2500000;
+const juliaHouseWidth = 5;
+const juliaHouseDepth = 11;
+const juliaHouseHeight = 8;
+const juliaGardenSize = 70;
+const juliaPaidAmount = 1000000;
+const peterVolumeInMeters =
+  peterHouseWidth * peterHouseDepth * peterHouseHeight;
 
-let peterHousePrice = peterVolumeInMeters * 2.5 * 1000 + peterHouseDetails[3] * 300;
-let juliaHousePrice = juliaVolumeInMeters * 2.5 * 1000 + juliaHouseDetails[3] * 300;
+const juliaVolumeInMeters =
+  juliaHouseWidth * juliaHouseDepth * juliaHouseHeight;
 
-if (peterHousePrice>peterHouseDetails[4]){
-    console.log("Peter, You bought this house for a cheaper price");
+const peterHousePrice =
+  peterVolumeInMeters * 2.5 * 1000 + peterGardenSize * 300;
+const juliaHousePrice =
+  juliaVolumeInMeters * 2.5 * 1000 + juliaGardenSize * 300;
+
+if (peterHousePrice > peterPaidAmount) {
+  console.log("Peter, You bought this house for a cheaper price");
+} else if (peterHousePrice === peterPaidAmount) {
+  console.log("Peter, You bought this house for correct price");
+} else {
+  console.log("Peter, You bought this house for higher price");
 }
-else if(peterHousePrice===peterHouseDetails[4]){
-    console.log("Peter, You bought this house for correct price");
-}
-else{
-    console.log("Peter, You bought this house for higher price");
-}
-if (juliaHousePrice>juliaHouseDetails[4]){
-    console.log("Julia, You bought this house for a cheaper price");
-}
-else if(juliaHousePrice===juliaHouseDetails[4]){
-    console.log("Julia, You bought this house for correct price");
-}
-else{
-    console.log("Julia, You bought this house for higher price");
+if (juliaHousePrice > juliaPaidAmount) {
+  console.log("Julia, You bought this house for a cheaper price");
+} else if (juliaHousePrice === juliaPaidAmount) {
+  console.log("Julia, You bought this house for correct price");
+} else {
+  console.log("Julia, You bought this house for higher price");
 }
 
 //Ez Namey (Startup name generator) Optional
-console.log("************ Ez Namey (Startup name generator) Optional *****************");
-let firstWords = ["Easy", "Awesome", "Corporate", "NextGen", "Innovative", "Creative", "CodeCraft", "Hackers", "Digital", "IT" ];
-let secondWords = ["Solutions", "World", "Themes", "Hands", "Providers", "Services", "Group", "Waves", "Ground", "Players"];
+console.log(
+  "************ Ez Namey (Startup name generator) Optional *****************"
+);
+const firstWords = [
+  "Easy",
+  "Awesome",
+  "Corporate",
+  "NextGen",
+  "Innovative",
+  "Creative",
+  "CodeCraft",
+  "Hackers",
+  "Digital",
+  "IT",
+];
+const secondWords = [
+  "Solutions",
+  "World",
+  "Themes",
+  "Hands",
+  "Providers",
+  "Services",
+  "Group",
+  "Waves",
+  "Ground",
+  "Players",
+];
 const firstName = firstWords[Math.floor(Math.random() * 10)];
 const secondName = secondWords[Math.floor(Math.random() * 10)];
-let startupName = firstName + " " + secondName;
-let nameLength = startupName.length;
-console.log('The startup: "' + startupName + '" contains '+ nameLength + ' characters');
-
-
-
-
-
-
-
+const startupName = firstName + " " + secondName;
+const nameLength = startupName.length;
+console.log(`The startup: "${startupName}" contains ${nameLength} characters`);
