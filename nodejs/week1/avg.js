2c80eaa05c3360cf7ade721c597924ca417b45f0
Saikiruthiga <saikiruthiga06@gmail.com>
2024-08-08 15:25:20 +0200

    Implemented feedback

--- nodejs/week1/avg.js
+++ nodejs/week1/avg.js
@@ -3,20 +3,26 @@
 let total = 0;
 let count = 0;
 let average;
+let num = [];
 
 for (let i = 2; i < process.argv.length; i++) {
-  let num = Number(process.argv[i]);
-  if (isNaN(num)) {
-    console.log(`All the arguments should be a number`);
+  let value = Number(process.argv[i]);
+
+  if (isNaN(value)) {
+    console.log("The arguments should be a number");
     return;
   } else {
-    total += num;
-    count++;
+    num.push(value);
   }
 }
+for (let i = 0; i < num.length; i++) {
+  total += num[i];
+  count++;
+}
+
 if (count > 0) {
   average = total / count;
-  console.log(`The average is ${average}`);
+  console.log(`The average is ${average} `);
 } else {
-  console.log("Please provide some numbers");
+  console.log("please provide some arguments in numbers");
 }
