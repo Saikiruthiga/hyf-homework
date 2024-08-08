cac4cf5f74fc85516a488f38da8083cbb5f75b2e
Saikiruthiga <saikiruthiga06@gmail.com>
2024-08-08 15:43:56 +0200

    Implemented the program using array functions as per the feedback

--- nodejs/week1/avg.js
+++ nodejs/week1/avg.js
@@ -1,28 +1,15 @@
 // Program for calculate the average of the numbers provided as command line arguments.
 
-let total = 0;
-let count = 0;
-let average;
-let num = [];
+const num = process.argv.slice(2).map(Number);
 
-for (let i = 2; i < process.argv.length; i++) {
-  let value = Number(process.argv[i]);
-
-  if (isNaN(value)) {
-    console.log("The arguments should be a number");
-    return;
-  } else {
-    num.push(value);
-  }
+if (num.some(isNaN)) {
+  console.log("The arguments should be a number");
+  return;
 }
-for (let i = 0; i < num.length; i++) {
-  total += num[i];
-  count++;
-}
-
-if (count > 0) {
-  average = total / count;
-  console.log(`The average is ${average} `);
+if (num.length > 0) {
+  const total = num.reduce((acc, curr) => acc + curr, 0);
+  const average = total / num.length;
+  console.log(`The average is ${average}`);
 } else {
-  console.log("please provide some arguments in numbers");
+  console.log("Please provide some arguments in numbers");
 }
